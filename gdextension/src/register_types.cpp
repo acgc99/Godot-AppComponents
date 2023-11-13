63069d657283f2252a2df9bd539d1b53b9d684d5
acgc99 <acgc99@outlook.es>
2023-11-13 21:45:39 +0100

    Code style revision

--- gdextension/src/register_types.cpp
+++ gdextension/src/register_types.cpp
@@ -1,11 +1,11 @@
 #include "register_types.h"
 
+#include "ac_animated_control.h"
 #include "ac_http_image.h"
-#include "ac_icon_button.h"
 #include "ac_icon.h"
+#include "ac_icon_button.h"
 #include "ac_page_container.h"
 #include "ac_round_clipping_container.h"
-#include "ac_animated_control.h"
 
 #include "gdextension_interface.h"
 #include "godot_cpp/core/class_db.hpp"
@@ -18,12 +18,12 @@ void initialize_ac(ModuleInitializationLevel p_level){
     if(p_level != MODULE_INITIALIZATION_LEVEL_SCENE){
         return;
     }
-    ClassDB::register_class<ACHTTPImage>();
-    ClassDB::register_class<ACPageContainer>();
-    ClassDB::register_class<ACIconButton>();
-    ClassDB::register_class<ACIcon>();
-    ClassDB::register_class<ACRoundClippingContainer>();
     ClassDB::register_class<ACAnimatedControl>();
+    ClassDB::register_class<ACHTTPImage>();
+    ClassDB::register_class<ACIcon>();
+    ClassDB::register_class<ACIconButton>();
+    ClassDB::register_class<ACPageContainer>();
+    ClassDB::register_class<ACRoundClippingContainer>();
 }
 
 void uninitialize_ac(ModuleInitializationLevel p_level){
